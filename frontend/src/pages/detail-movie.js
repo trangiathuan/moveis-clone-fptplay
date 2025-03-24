86235e71483072791de6a152816fe287526545cd
Vũ Ngọc Quang Minh <minhngaongo1003@gmail.com>
2025-03-24 16:22:04 +0700

    lol

--- frontend/src/pages/detail-movie.js
+++ frontend/src/pages/detail-movie.js
@@ -1,6 +1,6 @@
-import Detail from "../components/deatail_movies/detail-movie";
+import Detail from "../components/detail_movies_component/detail-movie";
 import Navbar from "../components/navbar";
-
+import Footer from "../components/footer";
 const DetailMovie = () => {
     return (
         <div className="bg-black">
