1dca84e4f2ec571aa96d0f69d04c75e47d3ef33d
Vũ Ngọc Quang Minh <minhngaongo1003@gmail.com>
2025-03-28 22:17:54 +0700

    đã fix câu lệnh đẹp hơn

--- frontend/src/components/detail_movies_component/detail-movie.js
+++ frontend/src/components/detail_movies_component/detail-movie.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import Video_comp from "./video_component";
 import Star_comp from "./star_comp";
 import ActionButtons from "./actions-buttons";
 import MovieDescription from "./movie_descriptions";
-const Detail = ({ movieId }) => {
+const Detail = () => {
   const [movieData, setMovieData] = useState(null);
-
+    const [videoSrc, setVideoSrc] = useState("");
+  const {movieID}= useParams();// Lấy id từ URL
+  
   useEffect(() => {
-    const fetchMovieData = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/api/get-by-movieID/1`);
-        const result = await response.json();
-        if (result.EC === 0 && result.Data.length > 0) {
-          setMovieData(result.Data[0]);
+                fetchMovieData();
+            }, [movieID]);
+
+  // Lấy dữ liệu từ API bằng Axios
+  const fetchMovieData = async () => {
+    try {
+        const result = await axios.get(`http://localhost:8080/api/get-by-movieID/${movieID}`);
+        if (result.data.EC === 0 && result.data.Data.length > 0) {
+            console.log(result.data);
+            setMovieData(result.data.Data[0]); // Cập nhật dữ liệu phim
+            setVideoSrc(result.data.Data[0].MovieFilePath); // Cập nhật URL video
         }
-      } catch (error) {
-        console.error("Error fetching movie data:", error);
-      }
-    };
+    } catch (error) {
+        console.error("Lỗi khi gọi API:", error);
+    }
+};
 
-    fetchMovieData();
-  }, [movieId]);
-
-  if (!movieData) {
+if (!movieData) {
     return <div className="text-white">Loading...</div>;
-  }
+}
     return(
       <div className="flex flex-col items-center bg-black min-h-screen text-white">
       {/* Container để căn chỉnh video và thông tin phim */}
@@ -59,7 +65,7 @@ const Detail = ({ movieId }) => {
   </div>
 
   {/* Right Section */}
-  <div className="p-8 rounded-lg gray-800">
+  <div className="p-8 rounded-lg">
     {/* Action Buttons */}
     <div className="flex items-center gap-6 text-white text-sm">
       <ActionButtons />
