19d56919e2f6cfd568cdfc7a64224f6bb25bedd2
trangiathuan <nakrothfaker@krothfakergmail.fakergmailcom>
2025-04-04 01:40:28 +0700

    update api get movie add api get list movies

--- backend/src/routes/movieRoute.js
+++ backend/src/routes/movieRoute.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const movieController = require('../controllers/movieController')
 
 router.get('/get-all-movies-new', movieController.getAllMovieNewController);
-router.get('/get-by-episode/:slugMovieName/:slugEpisode', movieController.getByEpisodeController);
-router.get('/get-by-slugMovieName/:slugMovieName', movieController.getBySlugMovieNameController);
-
+router.get('/get-by-slugMovieName/:slugMovieName/:slugEpisode?', movieController.getBySlugMovieNameController);
 router.get('/get-by-categoryName/:slugCategoryName', movieController.getByCategoryController);
+router.get('/get-list-Movies/:slugMovieName', movieController.getListMoviesController);
 
 
 
