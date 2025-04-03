3989d551073387d0918a35689682d8b9a2b8b58b
MinhQui-Jei <minhqui2003@gmail.com>
2025-04-03 20:06:57 +0700

    fix css login

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import API from "../configs/endpoint";
+import bgImage from "../asset/image-logo/login-tablet.jpg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -9,8 +10,14 @@ const Login = () => {
   const [isChecked, setIsChecked] = useState(false);
   const [step, setStep] = useState(1);
   const [countdown, setCountdown] = useState(30);
-  const [isCounting, setIsCounting] = useState(false); // Chỉ chạy khi bấm "Gửi lại mã"
+  const [isCounting, setIsCounting] = useState(false);
   const navigate = useNavigate();
+
+  const maskEmail = (email) => {
+    const emailParts = email.split('@');
+    const maskedEmail = emailParts[0].slice(0, 4) + '****' + '@' + emailParts[1];
+    return maskedEmail;
+  };
 
   useEffect(() => {
     if (!isCounting || countdown <= 0) {
@@ -49,7 +56,7 @@ const Login = () => {
       }, 500);
     } else {
       console.log(res.data);
-      alert(res.data.Data.Message)
+      alert(res.data.Data.Message);
     }
   };
 
@@ -61,25 +68,28 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full min-h-screen bg-black flex flex-col">
-      <div className="flex items-center">
-        <div className="flex flex-grow justify-center w-16 h-16">
-          <img src={require("../asset/image-logo/logoFPT.png")} alt="Logo FPT" />
+    <div className="relative w-full min-h-screen flex flex-col bg-cover bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
+
+      <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-lg"></div>
+
+      <div className="relative flex items-center">
+        <div className="flex flex-grow justify-center">
+          <img className="w-56 h-auto mr-4" src={require("../asset/image-logo/logoFPT.png")} alt="Logo FPT" />
         </div>
         <a
           href="/"
-          className="flex justify-center items-center w-9 h-9 text-white text-black pe-8 text-4xl font-bold rounded-full shadow-md"
+          className="flex justify-center items-center w-9 h-9 text-black  text-2xl bg-neutral-500 font-bold rounded-full shadow-md"
         >
           x
         </a>
       </div>
 
-      <div className="flex items-center justify-center flex-grow">
+      <div className="relative z-10 flex flex-col items-center justify-center flex-grow mb-28">
         <div className="bg-neutral-800 p-8 rounded-xl shadow-lg w-full max-w-lg">
           {step === 1 && (
             <>
-              <h2 className="text-3xl text-white font-bold mb-10 text-center">Đăng nhập hoặc đăng ký</h2>
-              <form className="space-y-7" onSubmit={handleContinue}>
+              <h2 className="text-3xl text-white font-bold mb-12 text-left ml-7 mt-5">Đăng nhập hoặc đăng ký</h2>
+              <form className="space-y-8 mr-8 ml-8" onSubmit={handleContinue}>
                 <input
                   type="email"
                   className="w-full p-3 text-neutral-400 bg-neutral-700 rounded-lg"
@@ -90,14 +100,13 @@ const Login = () => {
                 />
                 <button
                   type="submit"
-                  className={`w-full py-3 rounded-lg font-medium ${isChecked ? "bg-orange-600 text-white" : "bg-neutral-700 text-neutral-400"
-                    }`}
+                  className={`w-full py-3 rounded-lg font-medium mt-4  ${isChecked ? "bg-orange-600 text-white" : "bg-neutral-700 text-neutral-400"}`}
                   disabled={!isChecked}
                 >
                   Tiếp Tục
                 </button>
               </form>
-              <div className="mt-4 text-center flex items-center justify-center space-x-2">
+              <div className="mt-7 mb-8 text-center flex items-center justify-center space-x-2">
                 <input type="checkbox" id="check" className="accent-orange-600" onChange={(e) => setIsChecked(e.target.checked)} />
                 <p className="text-gray-600 text-sm">
                   Tôi đã đọc và đồng ý với{" "}
@@ -111,31 +120,45 @@ const Login = () => {
 
           {step === 2 && (
             <>
-              <h2 className="text-3xl text-white font-bold mb-10 text-center">Nhập mã OTP</h2>
-              <p className="text-gray-400 text-sm mb-4">Vui lòng kiểm tra email của bạn để nhận mã OTP</p>
-              <form className="space-y-7" onSubmit={handleSubmitOtp}>
-                <input
-                  type="text"
-                  className="w-full p-3 text-neutral-400 bg-neutral-700 rounded-lg"
-                  placeholder="Nhập OTP"
-                  value={otp}
-                  onChange={(e) => setOtp(e.target.value)}
-                  required
-                />
-                <button type="submit" className="w-full text-white bg-orange-600 py-3 rounded-lg font-medium">
-                  Xác Nhận
+              <h2 className="text-3xl text-white font-bold mb-12 text-left mt-5 ml-8">Xác thực mã OTP</h2>
+              <p className="text-gray-400 text-base mb-4 ml-8">
+                Nhấn nút <b className="font-black text-white">Gửi Lại Mã OTP</b> nếu bạn không nhận được mã về{" "}
+                <span className="font-bold text-white">{email ? maskEmail(email) : "Email chưa được nhập"}</span>.
+              </p>
+
+              <form className="space-y-8 mr-8 ml-8" onSubmit={handleSubmitOtp}>
+                <div className="flex flex-grow justify-center">
+                  <input
+                    type="text"
+                    className="w-full p-3 text-neutral-400 bg-neutral-700 rounded-lg w-3/4"
+                    placeholder="Nhập OTP"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                    required
+                  />
+                  <div className="mt-4 text-center">
+                    {isCounting ? (
+                      <span className="text-gray-500">Gửi lại mã sau {countdown}s</span>
+                    ) : (
+                      <a
+                        href="#"
+                        className="text-orange-600 w-full text-white bg-orange-600 py-3 rounded-lg font-medium ml-4"
+                        onClick={handleResend}
+                      >
+                        Gửi lại mã
+                      </a>
+                    )}
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className={`w-full py-3 rounded-lg font-medium mt-4  ${otp ? "bg-orange-600 text-white" : "bg-neutral-700 text-neutral-400"}`}
+                  disabled={!otp}  // Disable button if OTP is empty
+                >
+                  Xác nhận
                 </button>
               </form>
-
-              <div className="mt-4 text-center">
-                {isCounting ? (
-                  <span className="text-gray-500">Gửi lại mã sau {countdown}s</span>
-                ) : (
-                  <a href="#" className="text-orange-600" onClick={handleResend}>
-                    Gửi lại mã
-                  </a>
-                )}
-              </div>
             </>
           )}
         </div>
@@ -145,3 +168,4 @@ const Login = () => {
 };
 
 export default Login;
+
--- frontend/src/configs/endpoint.js
+++ frontend/src/configs/endpoint.js
@@ -1,4 +1,4 @@
-//const API = 'http://localhost:8080/api'
-const API = 'http://192.168.1.10:8080/api'
+const API = 'http://localhost:8080/api'
+//const API = 'http://192.168.1.10:8080/api'
 //const API = 'https://michigan-den-logistics-bali.trycloudflare.com/api/'
 export default API;
